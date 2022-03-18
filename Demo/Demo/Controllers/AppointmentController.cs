e3957f6ed6d5a0edc8593f6d3fff980f7e5a8ec1
Andreea Popa <andreea_popa22@yahoo.ro>
2022-03-18 13:25:45 +0200

    create table for appointments

---
+++ Demo/Demo/Controllers/AppointmentController.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            var entities = new popaadbEntities();
+            return View(entities.Appointments.ToList());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## ASP .Net MVC Medical Appointemnts Management App
+This app was developed during my induction period for my Junior .Net Software Developer position.
+
+### Summary:
+This projects manages medical appointments for pacients. The process is intended to be used by medical staff during telephonic conversations with patients.
