3784c064a1183182479942106f6da4c293c3cd0c
Andreea Popa <andreea_popa22@yahoo.ro>
2022-03-22 23:13:51 +0200

    show app with patient details done

--- Demo/Demo/Models/Patient.cs
+++ Demo/Demo/Models/Patient.cs
@@ -11,7 +11,7 @@ namespace Demo.Models
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Patient
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
