e595a6ba5a8aad4e198db677aaf059fc8acafb98
Andreea Popa <andreea_popa22@yahoo.ro>
2022-03-25 12:30:09 +0200

    footer style done

--- Demo/Demo/App_Start/BundleConfig.cs
+++ Demo/Demo/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Demo
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "/Content/site.css"));
         }
     }
 }
