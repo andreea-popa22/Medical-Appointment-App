619d6496c007c75a66bed3ee2d6f5393fb23154e
Andreea Popa <andreea_popa22@yahoo.ro>
2022-03-28 11:44:45 +0300

    patients panel in home page

--- Demo/Demo/Controllers/HomeController.cs
+++ Demo/Demo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,10 @@ namespace Demo.Controllers
 {
     public class HomeController : Controller
     {
+        private popaadbEntities entities = new popaadbEntities();
         public ActionResult Index()
         {
+            ViewBag.Patients = ListPatients();
             return View();
         }
 
@@ -25,6 +28,23 @@ namespace Demo.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [NonAction]
+        public List<Demo.Models.Patient> ListPatients()
+        {
+            var patientsList = new List<Demo.Models.Patient>();
+            var patients = from p in entities.Patients
+                           select p;
+            //foreach (var patient in patients)
+            //{
+            //    patientsList.Add(new Patient
+            //    {
+            //        Value = patient.PatientId.ToString(),
+            //        Text = patient.Name.ToString() + " " + patient.Surname.ToString()
+            //    });
+            //}
+            return patients.ToList();
         }
     }
 }
